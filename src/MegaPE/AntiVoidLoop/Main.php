42f394cb7ebad7e6e2a67d166352774189c4032a
Darunia18 <darunia1818@gmail.com>
2015-01-30 12:15:46 -0500

    Create Main.php

---
+++ src/MegaPE/AntiVoidLoop/Main.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MegaPE\AntiVoidLoop;
+
+use pocketmine\entity\Entity;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener{
+    
+    private $x;
+    private $y;
+    private $z;
+    private $startingHeight;
+    
+    public function onEnable(){
+        $config = $this->getConfig();
+        $this->x = $config->get("x");
+        $this->y = $config->get("y");
+        $this->z = $config->get("z");
+        $this->startingHeight = $config->get("starting-height");
+    }
+    
+    public function onVoidLoop(PlayerMoveEvent $event){
+        $player = $event->getPlayer();
+        $level = $player->getLevel();
+        
+        if($player->getY() <= $this->startingHeight){
+            $spawn = new Position($this->x,$this->y,$this->z,$level);
+            $player->teleport($spawn);
+        }
+    }
+}
