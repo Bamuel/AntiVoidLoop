211da6ca73d3606460d5caee009f34af015b33c7
Samuel <Samuel_ipad2@hotmail.com>
2015-02-08 15:14:38 +1100

    Update Main.php

--- src/MegaPE/AntiVoidLoop/Main.php
+++ src/MegaPE/AntiVoidLoop/Main.php
@@ -6,17 +6,24 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
 
 class Main extends PluginBase implements Listener{
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
+        $config = $this->getConfig();
+        $this->startingHeight = $config->get("starting-height");
     }
     
     public function onVoidLoop(PlayerMoveEvent $event){
-        if($event->getTo()->getFloorY() < 1){
+        if($event->getTo()->getFloorY() < $this->startingHeight){
             $event->setCancelled(true);
-            $event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSpawnLocation());
-            $event->getPlayer()->sendMessage(TextFormat::AQUA . "You were saved from the deadly \"Void Lood\"");
+            $player = $event->getPlayer();
+            $player->teleport($this->getServer()->getDefaultLevel()->getSpawn());
+            $player->sendMessage(TextFormat::AQUA . "You were saved from the deadly \"Void Loop\"");
         }
     }
 }
